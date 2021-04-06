4893f093319fd767fd22ed4bf564ddfabcb736da
LBlend <lblend@protonmail.com>
2021-04-06 02:55:54 +0200

    added video only channel cog

---
+++ src/cogs/camera_channel.py
@@ -0,0 +1,25 @@
+from discord.ext import commands
+
+import asyncio
+
+
+class CameraChannel(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def on_voice_state_update(self, member, before, after):
+        if after.channel is None:
+            return
+
+        if member.voice.channel.id != 817125310491131949:
+            return
+
+        if not member.voice.self_video:
+            await asyncio.sleep(10)
+            if not member.voice.self_video:
+                await member.move_to(channel=None)
+
+
+def setup(bot):
+    bot.add_listener(CameraChannel(bot).on_voice_state_update, 'on_voice_state_update')
+    bot.add_cog(CameraChannel(bot))
