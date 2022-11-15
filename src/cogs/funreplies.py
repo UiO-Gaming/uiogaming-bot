7a8e8818228f2590a675db695be2a00b19c242b8
LBlend <lblend@protonmail.com>
2022-11-15 02:18:40 +0100

    refactor & fix funreplies cog

--- src/cogs/funreplies.py
+++ src/cogs/funreplies.py
@@ -1,31 +1,30 @@
-from discord.ext import commands
-
 import re
+
+import discord
+from discord.ext import commands
 
 
 class FunReplies(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    async def react(self, message):
+    async def reply_to_triggers(self, message: discord.Message):
         if message.author.bot:
             return
 
-        if re.findall(r'(^|\W)borgerlønn(\W|$)', message.clean_content.lower()) != []:
+        message_content = message.content.lower()
+
+        if re.search(r'(^|\W)borgerlønn(\W|$)', message_content, flags=re.IGNORECASE):
             await message.channel.send('<@267415183931080715> DE SNAKKER OM BORGERLØNN')
 
-        elif re.findall(r'(^|\W)olof palme(\W|$)', message.clean_content.lower()) != []:
+        elif re.search(r'(^|\W)olof palme(\W|$)', message_content, flags=re.IGNORECASE):
             await message.channel.send('Jeg vet hvem som drepte Olof Palme 👀')
 
-        #  The Ultimate spaghetti
-        elif (
-            re.match(r'(\W|\s)*lesgo{1}(\s|\W)*', message.clean_content.lower()) or
-            message.clean_content.lower() == 'lesgo'
-        ):
+        elif re.search(r'(\W|\s)*le+s+go+(\s|\W)*', message_content):
             await message.channel.send('https://cdn.discordapp.com/attachments/750052141346979850/' +
                                        '824764933513281596/3xhpwbakz2361.png')
 
 
 async def setup(bot):
-    bot.add_listener(FunReplies(bot).react, 'on_message')
+    bot.add_listener(FunReplies(bot).reply_to_triggers, 'on_message')
     await bot.add_cog(FunReplies(bot))
