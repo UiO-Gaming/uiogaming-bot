c4be795048a8d8bf371c7ed5be1b91654aba1478
LBlend <lblend@protonmail.com>
2022-11-15 01:07:13 +0100

    always fetch non-cached version of channel

--- src/cogs/repeated_messages.py
+++ src/cogs/repeated_messages.py
@@ -6,26 +6,28 @@ class RepeatedMessages(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-        uio_gaming = self.bot.get_guild(747542543750660178)
-        self.uiog_general = uio_gaming.get_channel(747542544291987597)
+        self.uiog_server = self.bot.get_guild(747542543750660178)
 
         @aiocron.crontab("0 0 * * 5")
         async def fredag():
-            await self.uiog_general.send(
+            channel = await self.uiog_server.fetch_channel(747542544291987597)
+            await channel.send(
                 "NU ÄR DET FREDAG!\n" + \
                 "https://cdn.discordapp.com/attachments/750052141346979850/851216786259181578/video0.mp4"
             )
 
         @aiocron.crontab("0 0 * * 1")
         async def mandag():
-            await self.uiog_general.send(
+            channel = await self.uiog_server.fetch_channel(747542544291987597)
+            await channel.send(
                 "ENDELIG MANDAG!\n\n" + \
                 "https://cdn.discordapp.com/attachments/678396498089738250/862853827278929940/hvorfor_de_rike_br_spises.mp4"
             )
 
         @aiocron.crontab("0 0 1 1 *")
         async def new_year():
-            await self.uiog_general.send(
+            channel = await self.uiog_server.fetch_channel(747542544291987597)
+            await channel.send(
                 "GODT NYTTÅR!\n\nNå, meld dere inn i UiO Gaming igjen <:ThisIsAThreat:874998234072903710>"
             )
 
