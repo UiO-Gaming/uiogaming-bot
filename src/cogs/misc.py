d1af4008e7f25e0e5b04704248fc162123119831
LBlend <lblend@protonmail.com>
2022-12-09 13:56:32 +0100

    remove kweeb command

--- src/cogs/misc.py
+++ src/cogs/misc.py
@@ -41,39 +41,6 @@ class Misc(commands.Cog):
 
     @app_commands.checks.bot_has_permissions(embed_links=True)
     @app_commands.checks.cooldown(1, 2)
-    @app_commands.command(name='kweeb', description='김정운, 최고도')
-    async def kweeb(self, interaction: discord.Interaction):
-        """
-        Call out kweebs
-
-        Parameters
-        ----------
-        interaction (discord.Interaction): Slash command context object
-        """
-
-        await interaction.response.send_message(
-            """
-Melding du nettopp sendte ga muligens følgende vibber for brukeren som brukte denne kommandoen:
-
-> KOREA NUMBER ONE!
-> OMG I LUUUUV JUNGKOK!!! 🥰😍🥰 STAN JUNGKOOK! 🤞🤞🤞 사랑해!!! ♥♥♥
-> JEG EEEEELSKER 김치
-> OP OP OP OPPAN GANGNAM STYLE!
-
-Så da kan man spørre seg selv? Hva faen skjedde med god gammeldags weebdom?
-Til vår skuffelse finnes den fortsatt, men ble det for mainstream å være weeb.
-Så da beveger massen seg til det neste hyperkapitalistiske øst-asiatiske staten med økonomisk boom.
-Så hvor ble da av weebsa? Denne videoen svarer ganske godt på det
-https://cdn.discordapp.com/attachments/448502155184439297/1027355652059832351/THEY_WENT_TO_KOREA.mp4
-
-Du har nå blitt kalt en kweeb. Det er over for deg.
-Våkne!
-https://cdn.discordapp.com/attachments/674726496878723082/1042631569396990004/kweeb_er_den_nye_weeb.png
-            """
-        )
-
-    @app_commands.checks.bot_has_permissions(embed_links=True)
-    @app_commands.checks.cooldown(1, 2)
     @app_commands.command(name='owo', description='Oversetter teksten din til owo (we_eb)')
     async def owo(self, interaction: discord.Interaction, tekst: str):
         """
