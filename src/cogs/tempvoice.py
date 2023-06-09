1be268a931196d371fcb264ba36fcd82a9e6bed2
LBlend <lblend@protonmail.com>
2023-06-10 03:07:16 +0900

    initial tempvoice cog

---
+++ src/cogs/tempvoice.py
@@ -0,0 +1,96 @@
+from datetime import datetime
+
+import discord
+from discord.ext import commands
+from discord.ext import tasks
+
+from cogs.utils import embed_templates
+
+
+class TempVoice(commands.Cog):
+    """Allows users to create a temporary channel that will be deleted after 5 minutes of inactivity"""
+
+    def __init__(self, bot: commands.Bot):
+        """
+        Parameters
+        ----------
+        bot (commands.Bot): The bot instance
+        """
+
+        self.bot = bot
+        self.temp_vc_channels = {}
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(
+        self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState
+    ):
+        """
+        Listen for disconnects from the temporary voice channels
+        """
+
+        channel = after.channel
+        if channel not in self.temp_vc_channels:
+            return
+
+        if len(channel.members) == 0:
+            self.temp_vc_channels[channel]["no_members_since"] = datetime.now()
+
+    @tasks.loop(minutes=1)
+    async def check_temp_vc_channels(self):
+        """
+        Check for temporary voice channels that have been inactive for 5 minutes
+        """
+
+        for channel, data in self.temp_vc_channels.copy().items():
+            if not data["no_members_since"]:
+                continue
+
+            if (datetime.now() - data["no_members_since"]).total_seconds() >= 300:
+                try:
+                    await channel.delete(reason="tempvoice kanal inaktiv i 5 minutter")
+                except discord.Forbidden:
+                    self.bot.logger.error(f"Failed to delete temporary voice channel {channel}")
+                del self.temp_vc_channels[channel]
+
+    @commands.cooldown(1, 300, commands.BucketType.guild)
+    @commands.command(name="tempvoice")
+    async def tempvoice(self, interaction: discord.Interaction, name: str, limit: int = 0):
+        """
+        Create a temporary voice channel
+
+        Parameters
+        ----------
+        interaction (discord.Interaction): The interaction object
+        name (str): The name of the channel
+        limit (int): The user limit of the channel
+        """
+
+        try:
+            channel = await interaction.guild.create_voice_channel(
+                name=name, user_limit=limit, reason="tempvoice kommando"
+            )
+        except discord.Forbidden:
+            self.bot.logger.error(f"Failed to create temporary voice channel in {interaction.guild}")
+            await interaction.response.send(
+                embed_templates.error_fatal(interaction, text="Jeg har ikke tilgang til å opprette en talekanal")
+            )
+            return
+
+        self.temp_vc_channels[channel] = {"created": datetime.now(), "no_members_since": None}
+
+        try:
+            await interaction.author.move_to(channel)
+        except discord.Forbidden:
+            pass
+
+
+async def setup(bot: commands.Bot):
+    """
+    Add the cog to the bot on extension load
+
+    Parameters
+    ----------
+    bot (commands.Bot): Bot instance
+    """
+
+    await bot.add_cog(TempVoice(bot))
