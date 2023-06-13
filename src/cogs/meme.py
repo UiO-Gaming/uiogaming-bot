5c306957eb8163d0022a6f6c53e57eb175ebcfd9
LBlend <lblend@protonmail.com>
2023-06-14 02:46:23 +0900

    initial crab rave command

--- src/cogs/meme.py
+++ src/cogs/meme.py
@@ -1,9 +1,14 @@
+import functools
+import os
 import textwrap
 from io import BytesIO
 
 import discord
 from discord import app_commands
 from discord.ext import commands
+from moviepy.editor import CompositeVideoClip
+from moviepy.editor import TextClip
+from moviepy.editor import VideoFileClip
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageEnhance
@@ -170,6 +175,83 @@ class Meme(commands.Cog):
 
         # Send image
         await interaction.followup.send(file=discord.File(output, "sivert_goodbad.jpg"))
+
+    @app_commands.checks.bot_has_permissions(embed_links=True)
+    @app_commands.checks.cooldown(1, 60)
+    @app_commands.command(name="crabrave", description="Generer en crab rave video basert på tekst")
+    async def crab_rave(self, interaction: discord.Interaction, topptekst: str, bunntekst: str):
+        """
+        Generates a crab rave video based on text
+
+        Parameters
+        ----------
+        interaction (discord.Interaction): The interaction
+        topptekst (str): The upper text
+        bunntekst (str): The lower text
+        """
+
+        await interaction.response.defer()
+
+        generation_task = functools.partial(Meme.make_crab, topptekst, bunntekst, interaction.user.id)
+        await self.bot.loop.run_in_executor(None, generation_task)
+
+        temp_file = f"./src/assets/temp/{interaction.user.id}_crab.mp4"
+        await interaction.followup.send(file=discord.File(temp_file))
+        try:
+            os.remove(temp_file)
+        except (FileNotFoundError, PermissionError):
+            self.bot.logger.warn(f"Failed to remove temporary file {temp_file}")
+            pass
+
+    @staticmethod
+    def make_crab(top_text: str, bottom_text: str, user_id: int):
+        """
+        Generates a crab rave video based on text
+        Is it really stolen code if the copilot stole it for me?
+
+        Parameters
+        ----------
+        top_text (str): The upper text
+        bottom_text (str): The lower text
+        user_id (int): The user ID
+        """
+
+        font_path = "Noto-Sans-Bold"
+        clip = VideoFileClip("./src/assets/misc/crab.mp4")
+
+        top_part = (
+            TextClip(top_text, fontsize=60, color="white", stroke_width=2, stroke_color="black", font=font_path)
+            .set_start(11.0)
+            .set_position(("center", 290))
+            .set_duration(26.0)
+        )
+        middle_part = (
+            TextClip(
+                "____________________",
+                fontsize=48,
+                color="white",
+                font=font_path,
+            )
+            .set_start(11.0)
+            .set_position(("center", "center"))
+            .set_duration(26.0)
+        )
+        bottom_part = (
+            TextClip(bottom_text, fontsize=60, color="white", stroke_width=2, stroke_color="black", font=font_path)
+            .set_start(11.0)
+            .set_position(("center", 380))
+            .set_duration(26.0)
+        )
+
+        video = CompositeVideoClip(
+            [clip, top_part.crossfadein(1), middle_part.crossfadein(1), bottom_part.crossfadein(1)]
+        ).set_duration(26.0)
+
+        video.write_videofile(
+            f"./src/assets/temp/{user_id}_crab.mp4", threads=1, preset="superfast", verbose=False, logger=None
+        )
+        clip.close()
+        video.close()
 
 
 async def setup(bot: commands.Bot):
