efcf0aec018e49db499a70159267dca0254ce092
LBlend <24893890+LBlend@users.noreply.github.com>
2023-11-24 03:58:19 +0100

    print log messages to stdout

--- src/logger.py
+++ src/logger.py
@@ -1,5 +1,6 @@
 import logging
 import logging.handlers
+import sys
 
 
 class BotLogger:
@@ -8,9 +9,14 @@ class BotLogger:
     def __init__(self):
         logger = logging.getLogger("discord")
         logger.setLevel(logging.INFO)
-        handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
         formatter = logging.Formatter("[{asctime}] [{levelname:<8}] {name}: {message}", "%Y-%m-%d %H:%M:%S", style="{")
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
+
+        file_handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+        console_handler = logging.StreamHandler(sys.stdout)  # level is INFO we use stdout instead of stderr
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
 
         self.logger = logger
