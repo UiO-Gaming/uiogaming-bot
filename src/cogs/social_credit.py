dc4e7229907f2742a5d023fcde142d1af496fbe1
LBlend <24893890+LBlend@users.noreply.github.com>
2023-11-24 03:03:50 +0100

    fully type and comment social credit cog

--- src/cogs/social_credit.py
+++ src/cogs/social_credit.py
@@ -55,6 +55,8 @@ class SocialCredit(commands.Cog):
         self.init_db()
 
     def init_db(self):
+        """Create the necessary tables for the social credit cog to work"""
+
         self.cursor.execute(
             """
             CREATE TABLE IF NOT EXISTS social_credit (
@@ -65,6 +67,8 @@ class SocialCredit(commands.Cog):
         )
 
     def add_new_citizen(func):
+        """Decorator that, when used, make sure that a user is inserted into the database before executing the function"""
+
         async def wrapper(*args, **kwargs):
             self = args[0]
             self.cursor.execute("SELECT user_id FROM social_credit WHERE user_id = %s", (args[1],))
@@ -75,16 +79,26 @@ class SocialCredit(commands.Cog):
         return wrapper
 
     def roll(percent: int = 50):
+        """Decorator that executes the function with a given percent chance"""
+
         def decorator(func):
             async def wrapper(*args, **kwargs):
-                if randint(0, 100) <= percent:  # 25% chance
+                if randint(0, 100) <= percent:
                     await func(*args, **kwargs)
 
             return wrapper
 
         return decorator
 
-    def _add_citizen(self, user_id):
+    def _add_citizen(self, user_id: int):
+        """
+        Adds a user to the database
+        
+        Parameters
+        ----------
+        user_id (int): The user's Discord ID
+        """
+
         self.cursor.execute(
             """
             INSERT INTO social_credit
@@ -96,6 +110,10 @@ class SocialCredit(commands.Cog):
 
     @tasks.loop(hours=24.0, reconnect=True)
     async def fuck_uwu(self):
+        """
+        Deducts points from people with the weeb role every 24 hours
+        """
+
         await self.bot.wait_until_ready()
 
         weeb_role = self.bot.get_guild(747542543750660178).get_role(803629993539403826)
@@ -103,7 +121,17 @@ class SocialCredit(commands.Cog):
             await self.social_punishment(weeb.id, 1, "weeb")
 
     @add_new_citizen
-    async def social_punishment(self, user_id, points, reason):
+    async def social_punishment(self, user_id: int, points: int, reason: str):
+        """
+        Deducts a given amount of points for a given user
+
+        Parameters
+        ----------
+        user_id (int): The user's Discord ID
+        points (int): The amount of points to deduct
+        reason (str): The reason for the deduction. This is for logging purposes.
+        """
+
         self.bot.logger.info(f"{points} points deducted from {user_id} ({reason})")
         self.cursor.execute(
             """
@@ -115,7 +143,17 @@ class SocialCredit(commands.Cog):
         )
 
     @add_new_citizen
-    async def social_reward(self, user_id, points, reason):
+    async def social_reward(self, user_id: int, points: int, reason: str):
+        """
+        Gives a given amount of points for a given user
+
+        Parameters
+        ----------
+        user_id (int): The user's Discord ID
+        points (int): The amount of points to add
+        reason (str): The reason for the addition. This is for logging purposes.
+        """
+
         self.bot.logger.info(f"{points} points given to {user_id} ({reason})")
         self.cursor.execute(
             """
@@ -130,6 +168,15 @@ class SocialCredit(commands.Cog):
 
     @social_credit_group.command(name="credits", description="Sjekk hvor dårlig menneske du er")
     async def credits(self, interaction: discord.Interaction, *, bruker: discord.Member | None = None):
+        """
+        Check the social credit score of a given user
+
+        Parameters
+        ----------
+        interaction (discord.Interaction): The interaction object
+        bruker (discord.Member | None): The user to check the score of. Defaults to the user who invoked the command
+        """
+
         if not bruker:
             bruker = interaction.user
 
@@ -148,6 +195,14 @@ class SocialCredit(commands.Cog):
 
     @social_credit_group.command(name="leaderboard", description="Sjekk hvem som er de beste og verste borgerne")
     async def leaderboard(self, interaction: discord.Interaction):
+        """
+        Gives an overview of the users with the highest and lowest social credit scores
+
+        Parameters
+        ----------
+        interaction (discord.Interaction): The interaction object
+        """
+
         await interaction.response.defer()
 
         self.cursor.execute(
@@ -194,7 +249,15 @@ class SocialCredit(commands.Cog):
         return embed
 
     @commands.Cog.listener("on_message")
-    async def on_message(self, message):
+    async def on_message(self, message: discord.Message):
+        """
+        Listens for messages and gives/takes points accordingly
+
+        Parameters
+        ----------
+        message (discord.Message): The message object
+        """
+
         if message.author.bot:
             return
 
@@ -205,37 +268,86 @@ class SocialCredit(commands.Cog):
         await self.night_owl(message)
 
     @roll(percent=50)
-    async def politcal_content(self, message):
+    async def politcal_content(self, message: discord.Message):
+        """
+        Punishes users for writing in the politics channel
+
+        Parameters
+        ----------
+        message (discord.Message): The message object
+        """
+
         if message.channel.id == 754706204349038644:
             await self.social_punishment(message.author.id, 25, "politics")
 
     @roll(percent=25)
-    async def chad_message(self, message):
+    async def chad_message(self, message: discord.Message):
+        """
+        Rewards users for writing in the member-chat channel
+
+        Parameters
+        ----------
+        message (discord.Message): The message object
+        """
+
         if message.channel.id == 811606213665357824:
             await self.social_reward(message.author.id, 10, "member-chat")
 
     @roll(percent=50)
-    async def early_bird(self, message):
+    async def early_bird(self, message: discord.Message):
+        """
+        Punishes users for writing between 05 AM and 10 AM
+
+        Parameters
+        ----------
+        message (discord.Message): The message object
+        """
+
         illegal_hours = [5, 6, 7, 8, 9]
 
         if message.created_at.hour in illegal_hours:
             await self.social_punishment(message.author.id, 10, "early-bird")
 
     @roll(percent=50)
-    async def night_owl(self, message):
+    async def night_owl(self, message: discord.Message):
+        """
+        Rewards users for writing between 01 AM and 05 AM
+
+        Parameters
+        ----------
+        message (discord.Message): The message object
+        """
+
         happy_hours = [1, 2, 3, 4]
 
         if message.created_at.hour in happy_hours:
             await self.social_reward(message.author.id, 10, "night-owl")
 
-    async def gullkorn(self, message):
+    async def gullkorn(self, message: discord.Message):
+        """
+        Listens for messages in the gullkorn channel and punishes the user if they are mentioned
+
+        Parameters
+        ----------
+        message (discord.Message): The message object
+        """
+
         if message.channel.id == 865970753748074576:
             if message.mentions:
                 for mention in message.mentions:
                     await self.social_punishment(mention.id, 10, "gullkorn")
 
     @commands.Cog.listener("on_reaction_add")
-    async def on_star_add(self, reaction, user):
+    async def on_star_add(self, reaction: discord.Reaction, user: discord.User | discord.Member):
+        """
+        Rewards users for having their message starred. Punishes users for starring their own message
+
+        Parameters
+        ----------
+        reaction (discord.Reaction): The reaction object
+        user (discord.User | discord.Member): The user who reacted
+        """
+
         if user.bot:
             return
 
@@ -248,7 +360,11 @@ class SocialCredit(commands.Cog):
                     await self.social_reward(user.id, 25 * len(reaction.message.reactions), "add new stars")
 
     @commands.Cog.listener("on_reaction_remove")
-    async def on_star_remove(self, reaction, user):
+    async def on_star_remove(self, reaction: discord.Reaction, user: discord.User | discord.Member):
+        """
+        Deducts points who have a star reaction removed from their message
+        """
+
         if user.bot:
             return
 
@@ -258,4 +374,12 @@ class SocialCredit(commands.Cog):
 
 
 async def setup(bot: commands.Bot):
+    """
+    Add the cog to the bot on extension load
+
+    Parameters
+    ----------
+    bot (commands.Bot): Bot instance
+    """
+
     await bot.add_cog(SocialCredit(bot))
